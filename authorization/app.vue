79aa78db88946a3b0c15f03c33e16778ad16c4b8
Namiko01 <vitaliknam01@gmail.com>
2023-06-30 13:40:54 +0300

    feat: Добавлены компоненты инпут и баттон, стилизованы и интегрированы на основную страницу. осталась проблема с пропсами

--- authorization/app.vue
+++ authorization/app.vue
@@ -1,5 +1,44 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="container">
+    <form action="" method="get" class="form">
+      <h1 class="form__title">Please sign in</h1>
+      <Input :placeholder="'login'"/>
+      <Input :placeholder="'password'"/>
+      <SubmitButton/>
+    </form>
   </div>
 </template>
+
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100;300&display=swap');
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  html{
+      font-family: roboto;
+      font-size: 16px;
+      font-weight: 100;
+  }
+  .container{
+    width: 100%;
+    min-height: 100vh;
+    background-color: #2b3035;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .form{
+    width: 350px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    color: #adb5bd;
+  }
+  .form__title{
+    margin-bottom: 10px;
+    font-weight: 300;
+  }
+</style>
---
+++ authorization/components/Input.vue
@@ -0,0 +1,29 @@
+<template>
+    <input type="text" class="input-login" placeholder='{{ placeholder }}'>
+</template>
+
+<style>
+    .input-login{
+        height: 58px;
+        width: 298px;
+        background-color: rgb(33, 37, 41);
+        color: #adb5bd;
+        font-size: 16px;
+        border: none;
+        outline: none;
+        border-radius: 5px;
+        margin-bottom: 3px;
+        padding: 0 15px;
+        transition: all ease-in-out 0.5s;
+    }
+    .input-login:focus{
+        border: 1px solid #6ea8fe ;
+        box-shadow: 0 0 0 0.25rem rgba(13,110,253,.25);
+    }
+</style>
+
+<script setup>
+    const props = defineProps({
+        placeholder: String,
+    })
+</script>
