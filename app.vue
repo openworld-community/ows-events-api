c77b3678ea3d1db9b6e0038db14c7ec5f15c959d
Evgenii Perminov <119697239+HugeLetters@users.noreply.github.com>
2023-06-30 16:13:33 +0200

    Folder restructure (#6)
    restructure

---
+++ app.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import '@/assets/base.pcss';
+import '@/assets/colors.pcss';
+
+useHead({
+	link: [
+		{
+			rel: 'stylesheet',
+			href: 'https://fonts.googleapis.com/css2?family=Roboto:wght@100;300&display=swap',
+			fetchpriority: 'high'
+		}
+	]
+});
+</script>
+<template>
+	<NuxtPage />
+</template>
---
+++ components/Input.vue
@@ -0,0 +1,50 @@
+<template>
+    <input :type="type" :class="class" :placeholder="placeholder" :value="value">
+</template>
+
+<style>
+    .input-login{
+        height: 58px;
+        width: 298px;
+        background-color: rgb(33, 37, 41);
+        color: #adb5bd;
+        font-size: 16px;
+        border: none;
+        outline: none;
+        border-radius: 5px;
+        margin-bottom: 3px;
+        padding: 0 15px;
+        transition: all ease-in-out 0.5s;
+    }
+    .input-login:focus{
+        border: 1px solid #6ea8fe ;
+        box-shadow: 0 0 0 0.25rem rgba(13,110,253,.25);
+    }
+    .input-button{
+        height: 58px;
+        width: 298px;
+        background-color: #0b5ed7;
+        color: white;
+        border: none;
+        outline: none;
+        border-radius: 5px;
+        cursor: pointer;
+        margin-top: 10px;
+        transition: all ease-in-out 0.3s;
+        font-family: roboto;
+        font-size: 18px;
+        font-weight: 300;
+    }
+    .input-button:hover{
+        background-color: #0950b9;
+    }
+</style>
+
+<script setup>
+    const props = defineProps({
+        type: String,
+        placeholder: String,
+        class: String,
+        value: String,
+    })
+</script>
