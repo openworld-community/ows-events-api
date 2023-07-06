ec7ec041e7dc8a914a0bff4c6f90a51177fd41e4
moonlightboss <cumrak.legenda@gmail.com>
2023-07-07 00:14:28 +0300

    Add: Footer

---
+++ components/footer.vue
@@ -0,0 +1,22 @@
+<template>
+	<footer class="footer">
+		<div class="container">
+			<div class="footer-info">
+				<p>OWS-EVENTS</p>
+				<p>Адрес: Ул Для Примера</p>
+				<p>Телефон: +7 123 456 7890</p>
+				<p>Email: info@owsevents.com</p>
+			</div>
+		</div>
+	</footer>
+</template>
+
+<style scoped>
+.footer {
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	background-color: #374259;
+    min-height: 200px;
+}
+</style>
