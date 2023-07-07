67ffe0686cbdc78964dc0bed1bcbc8dfddf1f311
Namiko01 <vitaliknam01@gmail.com>
2023-07-07 18:33:28 +0300

    fix: уделен main.css , импорты из него перенесены в base.pcss. В base.pcss удалены сбросы из tailwind, которые были прописаны вручную. В package.json удалена неизвестная зависимость. В nuxt.config изменен путь к base.pcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	css: ['~/assets/main.css'],
-  	postcss: {
+	css: ['~/assets/base.pcss'],
+	postcss: {
 		plugins: {
 		tailwindcss: {},
 		autoprefixer: {},
 		},
-  	},
-  })
+	},
+})
