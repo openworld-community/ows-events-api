6e9fbb5a229a85e1ea3e4ff5fedec05be7d3bd45
Huge Letters <ckuka71@gmail.com>
2023-07-07 10:26:43 +0200

    init

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+        node: true
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:vue/vue3-essential',
+        'plugin:nuxt/base',
+        'plugin:nuxt/recommended',
+        '@nuxt/eslint-config'
+    ],
+    overrides: [],
+    parser: 'vue-eslint-parser',
+    parserOptions: {
+        parser: '@typescript-eslint/parser',
+        sourceType: 'module'
+    },
+    plugins: [],
+    rules: {
+        'vue/html-indent': ['error', 4],
+        'vue/singleline-html-element-content-newline': 'off',
+        'vue/multi-word-component-names': 'off'
+    }
+};
--- pages/index.vue
+++ pages/index.vue
@@ -4,41 +4,45 @@ import { Route } from '@/types/router';
 const routes = Object.keys(Route);
 </script>
 <template>
-	<div class="root">
-		<h1>HOMEPAGE</h1>
-		<nav>
-			<ol>
-				<li v-for="route in routes">
-					<NuxtLink
-						:to="{ name: route }"
-						class="route-link"
-						>{{ route }}</NuxtLink
-					>
-				</li>
-			</ol>
-		</nav>
-	</div>
+    <div class="root">
+        <h1>HOMEPAGE</h1>
+        <nav>
+            <ol>
+                <li
+                    v-for="route in routes"
+                    :key="route"
+                >
+                    <NuxtLink
+                        :to="{ name: route }"
+                        class="route-link"
+                    >
+                        {{ route }}
+                    </NuxtLink>
+                </li>
+            </ol>
+        </nav>
+    </div>
 </template>
 <style scoped lang="postcss">
 .root {
-	min-height: 100dvh;
-	text-align: center;
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	justify-content: center;
-	font-size: 1.5rem;
+    min-height: 100dvh;
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-size: 1.5rem;
 }
 h1 {
-	font-size: 1.7rem;
-	font-weight: 800;
-	margin-bottom: 0.5rem;
+    font-size: 1.7rem;
+    font-weight: 800;
+    margin-bottom: 0.5rem;
 }
 .route-link {
-	transition: text-shadow 200ms;
-	&:hover,
-	&:focus {
-		text-shadow: 2px 2px 3px var(--color-light);
-	}
+    transition: text-shadow 200ms;
+    &:hover,
+    &:focus {
+        text-shadow: 2px 2px 3px var(--color-light);
+    }
 }
 </style>
