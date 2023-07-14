bb2a53b602f337bc6091f4973851a8d2b4c86beb
moonlightboss <cumrak.legenda@gmail.com>
2023-07-14 04:55:42 +0300

    Add open/close header class

--- components/header.vue
+++ components/header.vue
@@ -1,9 +1,21 @@
 <script setup lang="ts">
+const isClosed = ref(true);
+onMounted(() => {
+    window.addEventListener('scroll', () => {
+        isClosed.value = window.scrollY > 0;
+    });
+});
 </script>
 
 <template>
-    <header class="header">
-        <NuxtLink to="/"><h1>OWS-EVENTS</h1></NuxtLink>
+    <header :class="{ 'header-open': !isClosed, 'header-closed': isClosed }">
+        <NuxtLink to="/">
+            <img
+                src=""
+                alt="logo"
+            />
+        </NuxtLink>
+
         <div class="nav-link">Пункт меню</div>
         /
         <div class="nav-link">Пункт меню</div>
@@ -11,7 +23,14 @@
         <div class="nav-link">Пункт меню</div>
         /
         <div class="nav-link">Пункт меню</div>
-        <NuxtLink to="/profile">Войти/профайл</NuxtLink> <!--сделать что бы менялось -->
+
+        <NuxtLink
+            class=""
+            to="/profile"
+        >
+            Войти
+        </NuxtLink>
+        /
         <NuxtLink
             to="/login"
             class="login-button"
@@ -20,8 +39,11 @@
         </NuxtLink>
     </header>
 </template>
+
 <style scoped>
-.header {
+header {
+    transition: height 0.3s ease;
+    height: 165px;
     background-color: #374259;
     padding: 20px;
     display: flex;
@@ -34,17 +56,22 @@
     z-index: 9999;
 }
 
-.header h1 {
+.header-closed {
+    height: 115px;
+}
+
+header h1 {
     color: #ffffff;
     font-size: 24px;
     margin: 0;
 }
-.header .nav-link {
+
+header .nav-link {
     color: #ffffff;
     font-size: 24px;
     margin: 0;
 }
-.header .login-button {
+header .login-button {
     border-radius: 24px;
     background: var(--gradient-primary-vertical, linear-gradient(180deg, #48c78e 0%, #21a86b 100%));
 
@@ -57,7 +84,8 @@
     align-items: center;
     gap: 10px;
 }
-.header .login-button:hover {
+
+header .login-button:hover {
     border-radius: 24px;
     background: var(--primary-green, #48c78e);
 
