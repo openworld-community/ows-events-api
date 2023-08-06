aa69d96593c51a9aeae28a26d1ff3db5996d0fb6
Liudmila Larionova <ritsumei@yandex.com>
2023-08-06 21:19:52 +0300

    feat: connect unplugin icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: ['@nuxtjs/eslint-module', 'nuxt-quasar-ui'],
+    modules: [
+        '@nuxtjs/eslint-module',
+        'nuxt-quasar-ui',
+        [
+            'unplugin-icons/nuxt',
+            {
+                compiler: 'vue3',
+                autoInstall: true
+            }
+        ]
+    ],
     css: ['~/assets/base.pcss'],
     postcss: {
         plugins: {
